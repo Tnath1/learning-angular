8bef2ba9f7f347dbecb78ea70816d838e9b4d5e9
Arome Jonathan Ukpoju <99257453+Tnath1@users.noreply.github.com>
2024-05-18 20:04:13 +0100

    prop binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,15 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   name = 'Arome';
-  printData(){
-    
+
+  imgUrl =
+    'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcR2v8jGQFEHwDE0bEIm2Sofs-0n5RUWyiNtY_JQw46IozVB-YPU';
+
+  printData() {
+    console.log('Hello');
   }
 }
