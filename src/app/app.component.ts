1defe1880a4a04ad526cea77f9dc71980a07bc8b
Arome Jonathan Ukpoju <99257453+Tnath1@users.noreply.github.com>
2024-05-18 21:45:02 +0100

    components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,27 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ProductComponent } from './product/product.component';
+import { ServicesComponent } from './services/services.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ProductComponent, ServicesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  name = 'Arome';
+  name: string = 'Arome';
+
+  isAvailable: boolean = true;
 
   imgUrl =
     'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcR2v8jGQFEHwDE0bEIm2Sofs-0n5RUWyiNtY_JQw46IozVB-YPU';
 
+  inputType = 'password';
+
   printData() {
     console.log('Hello');
+
+    this.isAvailable = !this.isAvailable;
   }
 }
