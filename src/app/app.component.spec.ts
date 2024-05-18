f831edb7a15c4daefe36f334ed608ca8d9c6f966
Arome Jonathan Ukpoju <99257453+Tnath1@users.noreply.github.com>
2024-05-18 19:25:57 +0100

    angular setup

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
   it(`should have the 'firstapp' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('firstapp');
+    expect(app.name).toEqual('Arome');
   });
 
   it('should render title', () => {
